c1fba9d17780ad35c01afe57cb17fae12c4350ce
Debosmita Pal <debosmitapal429@gmail.com>
2025-10-03 20:27:29 +0530

    modified contact

--- frontend/src/components/Contacts.jsx
+++ frontend/src/components/Contacts.jsx
@@ -23,7 +23,7 @@ function Contact() {
     setStatus("");
 
     try {
-      const response = await fetch("http://localhost:5000/contact" || url, {
+      const response = await fetch(`${url}/contact`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
